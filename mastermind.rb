8f98f653fa3765b0731f7b06f13923dc16dabc17
Pouyan Chamanian <pouyannc@gmail.com>
2022-10-11 19:13:03 -0400

    Full game with intro and replay screen

--- mastermind.rb
+++ mastermind.rb
@@ -1,34 +1,40 @@
-# computer generates random 4 color code from 6 possible colors [g, b, r, y, o, p]
-# player has 12 turns to guess correct code
-# each turn return the number of correct guesses in correct spot and correct guesses in incorrect spot
-
 class Mastermind
-  @@COLORS = ["g", "b", "r", "y", "o", "p"]
+  @@DIGITS = ["1", "2", "3", "4", "5", "6"]
   @@reduced_computer_code
 
-  attr_accessor :player_guess, :computer_code
+  attr_accessor :player_guess, :computer_code, :start
 
   def initialize 
     @computer_code = get_random_code
     @player_guess
+    @start = false
     @@reduced_computer_code = @computer_code.clone
   end
 
   def get_random_code
     random_code = []
-    (1..4).each {random_code.push(@@COLORS.sample)}
+    (1..4).each {random_code.push(@@DIGITS.sample)}
     return random_code
   end
 
   def get_player_guess
-    puts "Please enter your guess (first letter of color [cccc])"
-    @player_guess = gets.chomp.downcase.split("")
+    puts "Enter guess (no spaces):"
+
+    loop do
+      @player_guess = gets.chomp.downcase.split("")
+      unless @player_guess.length == 4 && @player_guess.all? {|c| c.to_i.between?(1,6)}
+        puts "Invalid response, please guess a 4 digit code using DIGITS 1-6 (no spaces)"
+      else
+        break
+      end
+    end
+
   end
 
-  def correct_c_and_p # color and position are both correct
+  def correct_c_and_p # digit and position are both correct
     n_correct = 0
-    @player_guess.each_with_index do |color, i|
-      if color == @computer_code[i]
+    @player_guess.each_with_index do |digit, i|
+      if digit == @computer_code[i]
         n_correct += 1 
         @@reduced_computer_code[i] = 0
       end
@@ -36,40 +42,66 @@ class Mastermind
     return n_correct
   end
 
-  def correct_c_only # only color is correct, not position
+  def correct_c_only # only digit is correct, not position
     n_correct = 0
-    @player_guess.each_with_index do |color, i|
-      if @@reduced_computer_code.any?(color)
+    @player_guess.each_with_index do |digit, i|
+      if @@reduced_computer_code[i] != 0 && @@reduced_computer_code.any?(digit)
         n_correct += 1
-        @@reduced_computer_code[@@reduced_computer_code.find_index(color)] = 0
+        @@reduced_computer_code[@@reduced_computer_code.find_index(digit)] = 0
       end
     end
     return n_correct
   end
 
-  def get_guess_response # return the [num correct colors in correct position, num correct colors in wrong position]
-    response = [correct_c_and_p, correct_c_only]
+  def get_guess_hint # return the [num correct DIGITS in correct position, num correct DIGITS in wrong position]
+    hint = [correct_c_and_p, correct_c_only]
     @@reduced_computer_code = @computer_code.clone
-    return response
+    return hint
+  end
+
+  def intro_screen
+    puts "\nGuess the secret 4 digit code! (only using DIGITS 1 to 6)"
+    puts "After each guess, you will get a hint about: "
+    puts "[number of correct digits in the correct position, number of correct digits in the incorrect position]"
+    puts "Start? [y/n]"
+    self.set_start
+  end
+
+  def set_start
+    @start = false
+    @start = true if gets.chomp.downcase == "y"
   end
   
 end
 
 game = Mastermind.new
-p game.computer_code
-puts "6 possible colors: g, b, y, r, p, o"
+game.intro_screen
 
+while game.start
+  game.computer_code = game.get_random_code
+  p game.computer_code
+  guesses_remaining = 12
 
-(0..11).each do
-  p game.get_player_guess
-  guess_response = game.get_guess_response
-  p guess_response
-
-  if guess_response[0] == 4
-    puts "you win!"
-    break
+  (0..11).each do
+    puts "\nGuesses remaining: #{guesses_remaining}"
+    game.get_player_guess
+    guess_hint = game.get_guess_hint
+    sleep 1
+    puts "Hint: #{guess_hint}"
+    sleep 1
+    if guess_hint[0] == 4
+      puts "\nYou win!"
+      break
+    end
+    guesses_remaining -= 1
   end
 
+  puts "\nYou lose." if guesses_remaining == 0
+  puts "The code was #{game.computer_code.join}"
+  puts "Play again? [y/n]"
+  game.set_start
 end
+
+
 
 
